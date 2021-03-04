7a643b79a531cd983b5dbb6e8cb59eb781409931
vasyl_khula <vasylkhula02@gmail.com>
2021-03-04 12:46:49 +0200

    fix CountryController

--- src/main/java/com/itacademy/controller/CountryController.java
+++ src/main/java/com/itacademy/controller/CountryController.java
@@ -2,14 +2,17 @@ package com.itacademy.controller;
 
 import com.itacademy.model.Country;
 import com.itacademy.service.CountryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 @RequestMapping("/country")
 public class CountryController {
 
@@ -20,11 +23,12 @@ public class CountryController {
         this.countryService = countryService;
     }
 
-    @GetMapping("/country/{id}")
-    public String countryInfo(@PathVariable Long id, ModelAndView mav){
+    @GetMapping("/{id}")
+    public String countryInfo(@PathVariable Long id, Model model){
 
         Country country=countryService.getById(id);
-        mav.addObject("country", country);
+        log.info("============ "+country.toString());
+        model.addAttribute("country", country);
         return "country-info";
     }
 }
