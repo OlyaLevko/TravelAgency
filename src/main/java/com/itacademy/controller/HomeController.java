e2732778fefe1a89613d5d00ad4e879e63e11f79
vasyl_khula <vasylkhula02@gmail.com>
2021-03-10 13:00:58 +0200

    solve merge conflicts

--- src/main/java/com/itacademy/controller/HomeController.java
+++ src/main/java/com/itacademy/controller/HomeController.java
@@ -1,21 +1,10 @@
 package com.itacademy.controller;
 
-<<<<<<< HEAD
 import com.itacademy.service.CountryService;
 import lombok.extern.slf4j.Slf4j;
-=======
-import com.itacademy.model.Order;
-import com.itacademy.model.User;
-import com.itacademy.repository.OrderRepository;
-import com.itacademy.repository.UserRepository;
-import com.itacademy.security.SecurityUser;
-import com.itacademy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
->>>>>>> master
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
