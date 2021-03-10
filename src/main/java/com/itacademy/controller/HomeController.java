c4ef3a387b770fabcf19ebce92a9a2821703d3e6
Olya Levko <olyagusar@ukr.net>
2021-03-10 13:17:48 +0200

    Added authority to the navbar

--- src/main/java/com/itacademy/controller/HomeController.java
+++ src/main/java/com/itacademy/controller/HomeController.java
@@ -1,21 +1,8 @@
 package com.itacademy.controller;
 
-import com.itacademy.model.Order;
-import com.itacademy.model.User;
-import com.itacademy.repository.OrderRepository;
-import com.itacademy.repository.UserRepository;
-import com.itacademy.security.SecurityUser;
-import com.itacademy.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
 
 
 @Controller
@@ -23,8 +10,6 @@ public class HomeController {
 
     @GetMapping(value = "/")
     public String hello(){
-        Authentication a = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(a.getPrincipal());
         return "index";
     }
 
