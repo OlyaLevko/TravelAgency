7b7139ebadcfcca14a175bac0a9d3448cde77078
Olya Levko <olyagusar@ukr.net>
2021-03-12 20:28:59 +0200

    change strategy for deleting user

--- src/main/java/com/itacademy/repository/UserRepository.java
+++ src/main/java/com/itacademy/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.itacademy.repository;
 
 
+import com.itacademy.model.Order;
 import com.itacademy.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 
     User getByEmail(String email);
+
+    List<Order> getActiveOrdersForUser(Long id);
 }
